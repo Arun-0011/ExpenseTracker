ba9802ce645e737938943159b4ccd148427a85f0
Nitro <arunattri499@gmail.com>
2024-06-28 00:31:56 +0530

    .

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,23 +28,54 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverLay(){
     showModalBottomSheet(
-        context: context, builder: (ctx) => const NewExpense());
+        isScrollControlled: true,
+        context: context,
+        builder: (ctx) =>
+            NewExpense(
+              onAddExpense: _addExpense,
+            ));
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar( SnackBar(
+      content: const Text('Expense deleted'),
+      action: SnackBarAction(label: 'Undo', onPressed: () {
+        setState(() {
+          _registeredExpenses.insert(expenseIndex, expense);
+        });
+      }),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expense found!'),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
         actions: [IconButton(onPressed: _openAddExpenseOverLay, icon: const Icon(Icons.add))],
       ),
       body: Column(
-        children: [
-          const Text('The chart'),
-          Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
-          )
-        ],
+        children: [const Text('The chart'), Expanded(child: mainContent)],
       ),
     );
   }
