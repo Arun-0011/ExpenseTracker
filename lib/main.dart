b9002e51e7f3f0b56b8e23c5448687045e6cca58
Nitro <arunattri499@gmail.com>
2024-06-28 17:37:23 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 50, 181));
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +15,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer)),
+        textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16)),
+      ),
       home: const Expenses(),
     );
   }
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,5 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../../models/expense.dart';
 
@@ -15,6 +15,11 @@ class ExpensesList extends StatelessWidget {
         itemCount: expenses.length,
         itemBuilder: (ctx, index) => Dismissible(
             key: ValueKey(expenses[index]),
+            background: Container(
+              color: Theme.of(context).colorScheme.error.withOpacity(.5),
+              margin: EdgeInsets.symmetric(
+                  horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+            ),
             onDismissed: (direction) {
               onRemoveExpense(expenses[index]);
             },
